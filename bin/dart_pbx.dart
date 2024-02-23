0434ccf77d2da9057412d2fbde23c9f7174bb22d
unknown <Administrator@IGLINTERNS5.zescocorp.zescoad.zesco.co.zm>
2024-02-23 12:53:00 +0200

    First commit

---
+++ bin/dart_pbx.dart
@@ -0,0 +1,5 @@
+import 'package:dart_pbx/dart_pbx.dart' as dart_pbx;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_pbx.calculate()}!');
+}
---
+++ test/dart_pbx_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_pbx/dart_pbx.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
