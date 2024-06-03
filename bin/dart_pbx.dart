7efd238fe2058b00cc0eb0b610ea77a8b80c23dc
unknown <Administrator@IGLINTERNS5.zescocorp.zescoad.zesco.co.zm>
2024-06-03 11:47:08 +0200

    correct casing of import files

--- bin/dart_pbx.dart
+++ bin/dart_pbx.dart
@@ -22,32 +22,36 @@ void main() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
 
   String? wsIp = env['WS_SERVER_PORT'];
-  int? wsPort =env['WS_SERVER_PORT']!=null? int.parse(env['WS_SERVER_PORT']!):null;
+  int? wsPort =
+      env['WS_SERVER_PORT'] != null ? int.parse(env['WS_SERVER_PORT']!) : null;
 
   String? secureTcpIp = env['SEC_TCP_SERVER_ADDRESS'];
-  int? secureTcpPort =env['SEC_TCP_SERVER_PORT']!=null? int.parse(env['SEC_TCP_SERVER_PORT']!):null;
- String? path_to_certificate_file = env['PATH_TO_CERTIFICATE_FILE_PEM'];
-   String? path_to_private_key_file = env['PATH_TO_PRIVATE_KEY_FILE_PEM'];
+  int? secureTcpPort = env['SEC_TCP_SERVER_ADDRESS'] != null
+      ? int.parse(env['SEC_TCP_SERVER_PORT']!)
+      : null;
+  String? path_to_certificate_file = env['PATH_TO_CERTIFICATE_FILE_PEM'];
+  String? path_to_private_key_file = env['PATH_TO_PRIVATE_KEY_FILE_PEM'];
 
   String? udpIp = env['UPD_SERVER_ADDRESS'];
-  int? udpPort = env['UDP_SERVER_PORT']!=null?int.parse(env['UDP_SERVER_PORT']!):null;
-
-
+  int? udpPort = env['UDP_SERVER_PORT'] != null
+      ? int.parse(env['UDP_SERVER_PORT']!)
+      : null;
 
   //SipServer sipServer =
-  if(udpIp!=null) {
+  if (udpIp != null) {
     SipServer(udpIp, udpPort!);
   }
   //wsSipServer wsSever =
-  if(wsIp!=null) {
+  if (wsIp != null) {
     wsSipServer(wsIp, wsPort!, udpIp!, udpPort!);
   }
 
   //wssSipServer(wssIp, wssPort, udpIp, udpPort);
 
   //wssSipServer(secureTcpIp, secureTcpPort, udpIp, udpPort);
-  if(secureTcpIp!=null) {
-    SecureTcpSipServer(secureTcpIp,secureTcpPort!,path_to_certificate_file!,path_to_private_key_file!);
+  if (secureTcpIp != null) {
+    SecureTcpSipServer(secureTcpIp, secureTcpPort!, path_to_certificate_file!,
+        path_to_private_key_file!);
   }
   // var ion_webscket = ion.SimpleWebSocket("wss://dev.zesco.co.zm:7881/ws");
   // await ion_webscket.connect();
